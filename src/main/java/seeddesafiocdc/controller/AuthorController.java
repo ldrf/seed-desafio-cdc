d82ac264d5e0edaa0123e95fbf85799ec7749762
ldrf <ld.rf92@gmail.com>
2023-09-01 00:24:37 -0300

    validação para restringir emails duplicados

--- src/main/java/seeddesafiocdc/controller/AuthorController.java
+++ src/main/java/seeddesafiocdc/controller/AuthorController.java
@@ -2,6 +2,8 @@ package seeddesafiocdc.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,16 +14,30 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import seeddesafiocdc.model.Author;
 import seeddesafiocdc.payload.AuthorRequest;
+import seeddesafiocdc.validator.ForbidsDuplicateDuplicateAuthorMailValidator;
 
 @RestController
 @RequestMapping("/author")
-//Total de carga da classe = 2
+//Total de carga da classe = 3
 public class AuthorController {
 
 	private final EntityManager em;
+	private final ForbidsDuplicateDuplicateAuthorMailValidator forbidsDuplicateDuplicateAuthorMailValidator;
 
-	public AuthorController(final EntityManager em) {
+	public AuthorController(final EntityManager em,
+			final ForbidsDuplicateDuplicateAuthorMailValidator forbidsDuplicateDuplicateAuthorMailValidator) {
 		this.em = em;
+		this.forbidsDuplicateDuplicateAuthorMailValidator = forbidsDuplicateDuplicateAuthorMailValidator;
+	}
+
+	// Método usado para log no request que chego ao controler.
+	// A idéia é realizar configurações adicionais que precisam
+	// ser colocadas na execução da request relativa a esse
+	// controler.
+	@InitBinder
+	public void init(final WebDataBinder binder) {
+		// 1
+		binder.addValidators(forbidsDuplicateDuplicateAuthorMailValidator);
 	}
 
 	@PostMapping(value = "/create")
